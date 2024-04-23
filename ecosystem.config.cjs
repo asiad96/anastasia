ce379bbe1f2cf0def0e75bfaf5c8a3b0a6f89527
Anastasia <adwarica96r@email.here>
2024-04-22 19:40:02 -0500

    installed pm2, created Production Server and config file, need to refactor

---
+++ ecosystem.config.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  apps: [
+    {
+      name: "app",
+      script: "./server.js",
+      watch: true
+    },
+    {
+      name: "email-server",
+      script: "./server/index.js",
+      watch: true
+    }
+  ]
+};
---
+++ server.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const PORT = process.env.PORT || 3000;
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const app = express();
+
+app.use(express.static('dist'));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
