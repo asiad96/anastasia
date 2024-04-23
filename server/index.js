de6d571c61f3a00e9b92c27ab486dc6eb3099864
Anastasia <adwarica96r@email.here>
2024-04-22 20:13:30 -0500

    update port

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const { error } = require('console');
 require('dotenv').config();
 
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.EMAIL_PORT || 3001;
 
 const app = express();
 app.use(cors());
